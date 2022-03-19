65c0a3b7895d888b679a3be6eb013d8554654c36
Stedman Dennis <stedmandennis@gmail.com>
2022-03-19 09:16:27 -0500

    changed testData id to numbers

--- constants/testData.ts
+++ constants/testData.ts
@@ -5,61 +5,61 @@ interface TestData {
   
 export const testData1: TestData = {
     nodes: [
-        {data:{id:'q0', final: false, start: true}},
-        {data:{id:'q1', final: true, start: false}},
-        {data:{id:'q2', final: true, start: false}}
+        {data:{id:'0', final: false, start: true}},
+        {data:{id:'1', final: true, start: false}},
+        {data:{id:'2', final: true, start: false}}
     ],
     edges: [
-        {data:{source: "q0", target: "q1", expression:"a"}},
-        {data:{source: "q1", target: "q0", expression:"a"}},
-        {data:{source: "q0", target: "q2", expression:"b"}},
-        {data:{source: "q2", target: "q0", expression:"b"}},
-        {data:{source: "q1", target: "q1", expression:"b"}},
-        {data:{source: "q2", target: "q1", expression:"a"}}
+        {data:{source: "0", target: "1", expression:"a"}},
+        {data:{source: "1", target: "0", expression:"a"}},
+        {data:{source: "0", target: "2", expression:"b"}},
+        {data:{source: "2", target: "0", expression:"b"}},
+        {data:{source: "1", target: "1", expression:"b"}},
+        {data:{source: "2", target: "1", expression:"a"}}
     ]
   }
 export const testData2: TestData = {
     nodes: [
-        {data:{id:'q0', final: true, start: true}},
-        {data:{id:'q1', final: false, start: false}},
-        {data:{id:'q2', final: false, start: false}},
-        {data:{id:'q3', final: false, start: false}}
+        {data:{id:'0', final: true, start: true}},
+        {data:{id:'1', final: false, start: false}},
+        {data:{id:'2', final: false, start: false}},
+        {data:{id:'3', final: false, start: false}}
     ],
     edges: [
-        {data:{source: "q0", target: "q1", expression:"a"}},
-        {data:{source: "q0", target: "q3", expression:"b"}},
-        {data:{source: "q1", target: "q0", expression:"a"}},
-        {data:{source: "q1", target: "q2", expression:"b"}},
-        {data:{source: "q2", target: "q3", expression:"a"}},
-        {data:{source: "q2", target: "q1", expression:"b"}},
-        {data:{source: "q3", target: "q0", expression:"b"}},
-        {data:{source: "q3", target: "q2", expression:"a"}},
+        {data:{source: "0", target: "1", expression:"a"}},
+        {data:{source: "0", target: "3", expression:"b"}},
+        {data:{source: "1", target: "0", expression:"a"}},
+        {data:{source: "1", target: "2", expression:"b"}},
+        {data:{source: "2", target: "3", expression:"a"}},
+        {data:{source: "2", target: "1", expression:"b"}},
+        {data:{source: "3", target: "0", expression:"b"}},
+        {data:{source: "3", target: "2", expression:"a"}},
     ]
   }
 export const testData3: TestData = {
     nodes: [
-        {data:{id:'q0', final: false, start: true}},
-        {data:{id:'q1', final: false, start: false}},
-        {data:{id:'q2', final: false, start: false}},
-        {data:{id:'q3', final: false, start: false}},
-        {data:{id:'q4', final: false, start: false}},
-        {data:{id:'q5', final: true, start: false}},
-        {data:{id:'q6', final: true, start: false}},
+        {data:{id:'0', final: false, start: true}},
+        {data:{id:'1', final: false, start: false}},
+        {data:{id:'2', final: false, start: false}},
+        {data:{id:'3', final: false, start: false}},
+        {data:{id:'4', final: false, start: false}},
+        {data:{id:'5', final: true, start: false}},
+        {data:{id:'6', final: true, start: false}},
     ],
     edges: [
-        {data:{source: "q0", target: "q1", expression:"0"}},
-        {data:{source: "q0", target: "q2", expression:"1"}},
-        {data:{source: "q1", target: "q3", expression:"0"}},
-        {data:{source: "q1", target: "q4", expression:"1"}},
-        {data:{source: "q2", target: "q5", expression:"0"}},
-        {data:{source: "q2", target: "q6", expression:"1"}},
-        {data:{source: "q3", target: "q3", expression:"0"}},
-        {data:{source: "q3", target: "q4", expression:"1"}},
-        {data:{source: "q4", target: "q5", expression:"0"}},
-        {data:{source: "q4", target: "q6", expression:"1"}},
-        {data:{source: "q5", target: "q3", expression:"0"}},
-        {data:{source: "q5", target: "q4", expression:"1"}},
-        {data:{source: "q6", target: "q6", expression:"0+1"}}
+        {data:{source: "0", target: "1", expression:"0"}},
+        {data:{source: "0", target: "2", expression:"1"}},
+        {data:{source: "1", target: "3", expression:"0"}},
+        {data:{source: "1", target: "4", expression:"1"}},
+        {data:{source: "2", target: "5", expression:"0"}},
+        {data:{source: "2", target: "6", expression:"1"}},
+        {data:{source: "3", target: "3", expression:"0"}},
+        {data:{source: "3", target: "4", expression:"1"}},
+        {data:{source: "4", target: "5", expression:"0"}},
+        {data:{source: "4", target: "6", expression:"1"}},
+        {data:{source: "5", target: "3", expression:"0"}},
+        {data:{source: "5", target: "4", expression:"1"}},
+        {data:{source: "6", target: "6", expression:"0+1"}}
     ]
   }
   
